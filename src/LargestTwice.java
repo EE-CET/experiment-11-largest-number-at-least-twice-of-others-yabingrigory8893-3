5f7c725bf69b22c11e241beb6c834dcb868ea5ca
yabingrigory8893 <yabingrigory3@gmail.com>
2026-04-10 01:40:59 -0700

    Update LargestTwice.java

--- src/LargestTwice.java
+++ src/LargestTwice.java
@@ -1,9 +1,38 @@
+import java.util.Scanner;
+
 public class LargestTwice {
-    
-        // TODO: Read n
-        // TODO: Read array elements
-        // TODO: Find the largest element and its index
-        // TODO: Check if largest >= 2 * every other element
-        // TODO: Print index or -1
-    
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            if (!sc.hasNextInt()) return;
+            int n = sc.nextInt();
+            int[] nums = new int[n];
+            int max = -1;
+            int maxIndex = -1;
+            for (int i = 0; i < n; i++) {
+                if (sc.hasNextInt()) {
+                    nums[i] = sc.nextInt();
+                    if (nums[i] > max) {
+                        max = nums[i];
+                        maxIndex = i;
+                    }
+                }
+            }
+            
+            boolean isValid = true;
+            for (int i = 0; i < n; i++) {
+                if (i != maxIndex && max < 2L * nums[i]) {
+                    isValid = false;
+                    break;
+                }
+            }
+            
+            if (isValid) {
+                System.out.println(maxIndex);
+            } else {
+                System.out.println(-1);
+            }
+        }
+    }
 }
+
